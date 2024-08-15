807d0fe27bdcc8c7081a5573cb72167da70f2c78
KotlinPunk <mak351218@gmail.com>
2024-08-15 13:14:41 +0500

    Сделанная домашняя работа по 10-ому спринту

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -7,8 +7,10 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/practicum/playlistmaker/TrackAdapter.kt
@@ -0,0 +1,43 @@
+package com.practicum.playlistmaker
+
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.RoundedCorner
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.google.android.material.internal.ViewUtils.dpToPx
+import com.practicum.playlistmaker.databinding.TrackItemBinding
+
+class TrackAdapter (private val trackList : ArrayList<Track>) : RecyclerView.Adapter<TrackAdapter.TrackHolder>() {
+
+    class TrackHolder(item : View) : RecyclerView.ViewHolder(item){
+        val binding = TrackItemBinding.bind(item)
+        fun bind(track: Track) = with (binding){
+            trackNameXml.text = track.trackName
+            artistNameXml.text = track.artistName
+            trackTimeXml.text = track.trackTime
+            Glide.with(itemView)
+                .load(track.artworkUrl100)
+                .centerCrop()
+                .placeholder(R.drawable.ic_placeholder)
+                .transform(RoundedCorners(itemView.resources.getDimensionPixelOffset(R.dimen.two_dp)))
+                .into(artworkXml)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_item, parent, false)
+        return TrackHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackHolder, position: Int) {
+        holder.bind(trackList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return trackList.size
+    }
+}
