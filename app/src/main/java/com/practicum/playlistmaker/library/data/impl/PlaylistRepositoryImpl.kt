f120ca662b4bb0bff9565c36067cedbb050eedc9
KotlinPunk <mak351218@gmail.com>
2025-06-25 02:29:31 +0500

    Сделанная домашняя работа по 23-ему спринту (изменения №5.1, задублировался код)

--- app/src/main/java/com/practicum/playlistmaker/library/data/impl/PlaylistRepositoryImpl.kt
+++ app/src/main/java/com/practicum/playlistmaker/library/data/impl/PlaylistRepositoryImpl.kt
@@ -138,23 +138,6 @@ class PlaylistRepositoryImpl(
         }
     }
 
-    override suspend fun deletePlaylistRepo(playlistId: Long?) {
-        appDatabase.playlistDao().deletePlaylist(playlistId)
-    }
-
-    override suspend fun editPlaylistRepo(
-        idPl: Long?,
-        namePl: String,
-        descriptionPl: String,
-        imagePl: String?
-    ) {
-        appDatabase.playlistDao().editPlaylist(
-            idPl,
-            namePl,
-            descriptionPl,
-            imagePl
-        )
-    }
 
     // Функция для преобразования списка Track ID в JSON строку
     private fun List<Long>.toJson(gson: Gson): String = gson.toJson(this)
